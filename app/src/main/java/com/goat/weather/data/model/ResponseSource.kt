34a651cff37d10ffbf18214e95ec03c5307ee55b
Guodong <guodong.working@gmail.com>
2023-03-30 11:55:30 +0800

    天气作业

---
+++ app/src/main/java/com/goat/weather/data/model/ResponseSource.kt
@@ -0,0 +1,4 @@
+package com.goat.weather.data.model
+
+class ResponseSource {
+}
--- app/src/main/java/com/goat/weather/ui/adapter/MainRecycleViewAdapter.kt
+++ app/src/main/java/com/goat/weather/ui/adapter/MainRecycleViewAdapter.kt
@@ -2,8 +2,6 @@ package com.goat.weather.ui.adapter
 
 import androidx.annotation.LayoutRes
 import androidx.databinding.ViewDataBinding
-import com.goat.weather.base.BaseBindingAdapter
-import com.goat.weather.base.BindingViewHolder
 
 class DefaultBindingAdapter(@param:LayoutRes @field:LayoutRes override val layoutRes: Int)
     : BaseBindingAdapter<Any, ViewDataBinding>()
